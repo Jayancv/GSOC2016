a71ad4de7a6a329f0a706625c60c56eebeb72553
dananjayamahesh <dananjayamahesh@gmail.com>
2016-05-27 11:25:55 +0530

    Add gsoc siddhi extension and ml streaming algo

---
+++ gsoc/siddhi/extension/math/src/main/java/org/gsoc/siddhi/extension/math/App.java
@@ -0,0 +1,13 @@
+package org.gsoc.siddhi.extension.math;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
