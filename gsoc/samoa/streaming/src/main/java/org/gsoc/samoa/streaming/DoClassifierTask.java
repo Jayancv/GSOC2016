329a14804f7a0454d94df5c92dc47ccd697de6e7
dananjayamahesh <dananjayamahesh@gmail.com>
2016-07-17 22:41:39 +0530

    Add DoClusteringTask

---
+++ gsoc/samoa/streaming/src/main/java/org/gsoc/samoa/streaming/DoClassifierTask.java
@@ -0,0 +1,72 @@
+package org.gsoc.samoa.streaming;
+
+import com.github.javacliparser.ClassOption;
+import com.github.javacliparser.FlagOption;
+import com.github.javacliparser.IntOption;
+import com.github.javacliparser.Option;
+import org.apache.samoa.tasks.Task;
+import org.apache.samoa.topology.impl.SimpleComponentFactory;
+import org.apache.samoa.topology.impl.SimpleEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by mahesh on 7/17/16.
+ */
+public class DoClassifierTask {
+
+    // TODO: clean up this class for helping ML Developer in SAMOA
+    // TODO: clean up code from storm-impl
+
+    // It seems that the 3 extra options are not used.
+    // Probably should remove them
+    private static final String SUPPRESS_STATUS_OUT_MSG = "Suppress the task status output. Normally it is sent to stderr.";
+    private static final String SUPPRESS_RESULT_OUT_MSG = "Suppress the task result output. Normally it is sent to stdout.";
+    private static final String STATUS_UPDATE_FREQ_MSG = "Wait time in milliseconds between status updates.";
+    private static final Logger logger = LoggerFactory.getLogger(DoTask.class);
+
+    /**
+     * The main method.
+     *
+     * @param args
+     *          the arguments
+     */
+    public static void main(String[] args) {
+        logger.info("In Main");
+        //args = new String[]({"org.gsoc.samoa.streaming.ClusteringEvaluation"};
+        // ArrayList<String> tmpArgs = new ArrayList<String>(Arrays.asList(args));
+
+        // args = tmpArgs.toArray(new String[0]);
+
+        FlagOption suppressStatusOutOpt = new FlagOption("suppressStatusOut", 'S', SUPPRESS_STATUS_OUT_MSG);
+
+        FlagOption suppressResultOutOpt = new FlagOption("suppressResultOut", 'R', SUPPRESS_RESULT_OUT_MSG);
+
+        IntOption statusUpdateFreqOpt = new IntOption("statusUpdateFrequency", 'F', STATUS_UPDATE_FREQ_MSG, 1000, 0,
+                Integer.MAX_VALUE);
+
+        Option[] extraOptions = new Option[] { suppressStatusOutOpt, suppressResultOutOpt, statusUpdateFreqOpt };
+
+        StringBuilder cliString = new StringBuilder();
+        for (String arg : args) {
+            logger.info(arg);
+            cliString.append(" ").append(arg);
+        }
+        logger.debug("Command line string = {}", cliString.toString());
+        System.out.println("Command line string = " + cliString.toString());
+
+        Task task;
+        try {
+            task = ClassOption.cliStringToObject(cliString.toString(), Task.class, extraOptions);
+            logger.info("Successfully instantiating {}", task.getClass().getCanonicalName());
+        } catch (Exception e) {
+            logger.error("Fail to initialize the task", e);
+            System.out.println("Fail to initialize the task" + e);
+            return;
+        }
+        task.setFactory(new SimpleComponentFactory());
+        task.init();
+        SimpleEngine.submitTopology(task.getTopology());
+    }
+
+}
---
+++ gsoc/samoa/streaming/src/main/java/org/gsoc/samoa/streaming/streams/MyClusteringStream.java
@@ -0,0 +1,10 @@
+package org.gsoc.samoa.streaming.streams;
+
+import org.apache.samoa.streams.clustering.ClusteringStream;
+
+/**
+ * Created by mahesh on 7/17/16.
+ */
+public class MyClusteringStream extends ClusteringStream{
+
+}
