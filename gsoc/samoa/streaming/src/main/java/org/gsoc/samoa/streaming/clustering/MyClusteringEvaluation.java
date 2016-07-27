cb58c5728eef8710622ba2d91466df5e3f79c124
dananjayamahesh <dananjayamahesh@gmail.com>
2016-07-27 15:20:56 +0530

    Add STreaming Cliustering Support

---
+++ gsoc/samoa/streaming/src/main/java/org/gsoc/samoa/streaming/clustering/MyClusteringEvaluation.java
@@ -0,0 +1,87 @@
+package org.gsoc.samoa.streaming.clustering;
+
+import org.apache.samoa.core.ContentEvent;
+import org.apache.samoa.core.Processor;
+import org.apache.samoa.evaluation.ClusteringEvaluationContentEvent;
+import org.apache.samoa.evaluation.ClusteringResultContentEvent;
+import org.apache.samoa.instances.Instance;
+import org.apache.samoa.learners.clusterers.ClusteringContentEvent;
+
+import org.apache.samoa.moa.cluster.Cluster;
+import org.apache.samoa.moa.cluster.Clustering;
+import org.gsoc.samoa.streaming.gsocexample.GsocDestinationProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by mahesh on 7/19/16.
+ */
+public class MyClusteringEvaluation implements Processor{
+    private static final long serialVersionUID = -6043613438148776446L;
+    private int processorId;
+    private static final Logger logger = LoggerFactory.getLogger(MyClusteringEvaluation.class);
+
+    String evalPoint;
+    MyClusteringEvaluation(String evalPoint){
+        this.evalPoint = evalPoint;
+    }
+    @Override
+    public boolean process(ContentEvent event) {
+
+        logger.info("Process");
+        if (event instanceof ClusteringContentEvent) {
+            logger.info(event.getKey()+""+evalPoint+"ClusteringContentEvent");
+            ClusteringContentEvent e= (ClusteringContentEvent)event;
+            Instance inst = e.getInstance();
+
+            int numAttributes=inst.numAttributes();
+          //  logger.info("Attribute Size: "+numAttributes);
+           /* for(int i=0;i<numAttributes;i++){
+                logger.info(inst.attribute(i).toString()+""+inst.value(i)+"");
+            }*/
+        }
+
+        else if(event instanceof ClusteringResultContentEvent){
+            logger.info(event.getKey()+""+evalPoint+"ClusteringResultContentEvent\n");
+            ClusteringResultContentEvent resultEvent = (ClusteringResultContentEvent)event;
+            Clustering clustering=resultEvent.getClustering();
+
+
+            int numClusters = clustering.size();
+            logger.info("Nunber of Clustering: "+numClusters);
+            for(int i=0;i<numClusters;i++){
+                Cluster cluster = clustering.get(i);
+
+                logger.info("++++++Cluster"+i+"+++++");
+                double []clusterCenter=cluster.getCenter();
+                for(int j=0;j<clusterCenter.length;j++){
+                    logger.info("Dim: "+j+":"+clusterCenter[j]);
+                }
+                logger.info("Cluster End\n");
+            }
+            logger.info("Clustering End\n\n\n");
+
+        }
+
+        else if(event instanceof ClusteringEvaluationContentEvent){
+            logger.info(event.getKey()+""+evalPoint+"ClusteringEvaluationContentEvent\n");
+        }
+        else{
+            logger.info(event.getKey()+""+evalPoint+"ContentEvent\n");
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onCreate(int id) {
+        this.processorId = id;
+    }
+
+    @Override
+    public Processor newProcessor(Processor p) {
+        MyClusteringEvaluation newEval = (MyClusteringEvaluation)p;
+        return newEval;
+    }
+
+}
--- gsoc/samoa/streaming/src/main/java/org/gsoc/samoa/streaming/streams/MyClusteringStream.java
+++ gsoc/samoa/streaming/src/main/java/org/gsoc/samoa/streaming/streams/MyClusteringStream.java
@@ -1,56 +1,112 @@
 package org.gsoc.samoa.streaming.streams;
 
-import org.apache.samoa.instances.Instance;
-import org.apache.samoa.instances.InstancesHeader;
+import com.github.javacliparser.IntOption;
+import org.apache.samoa.instances.*;
+import org.apache.samoa.moa.core.DataPoint;
 import org.apache.samoa.moa.core.Example;
+import org.apache.samoa.moa.core.InstanceExample;
 import org.apache.samoa.moa.core.ObjectRepository;
 import org.apache.samoa.moa.options.AbstractOptionHandler;
 import org.apache.samoa.moa.tasks.TaskMonitor;
 import org.apache.samoa.streams.InstanceStream;
 import org.apache.samoa.streams.clustering.ClusteringStream;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * Created by mahesh on 7/17/16.
  */
 public class MyClusteringStream  extends ClusteringStream {
 
+    protected InstancesHeader streamHeader;
+    private int numGeneratedInstances;
+    private int nextEventCounter;
+    LinkedList<DataPoint> points = new LinkedList<DataPoint>();
+
+    LinkedList<double[]>cepEvents;
+    double [] values; //Cep Event
+    private int numAttributes;
+
+    public IntOption numClusterOption = new IntOption("numCluster", 'K',
+            "The average number of centroids in the model.", 5, 1, Integer.MAX_VALUE);
+
     @Override
     protected void prepareForUseImpl(TaskMonitor taskMonitor, ObjectRepository objectRepository) {
+        taskMonitor.setCurrentActivity("Preparing random RBF...", -1.0);
+        this.numAttributes =this.numAttsOption.getValue();
+        generateHeader();
+        restart();
+        values = new double[numAttributes];
 
+        for(int i=0;i<numAttributes;i++){
+            values[i]=i;
+        }
     }
 
     @Override
     public InstancesHeader getHeader() {
-        return null;
+        return streamHeader;
+        //return null;
     }
 
     @Override
     public long estimatedRemainingInstances() {
-        return 0;
+        return -1;
     }
 
     @Override
     public boolean hasMoreInstances() {
-        return false;
+        return true;
     }
 
     @Override
     public Example<Instance> nextInstance() {
-        return null;
+
+        numGeneratedInstances++;
+        double[] values_new = new double[numAttsOption.getValue()]; // +1
+        //double[] values = null;
+        int clusterChoice = -1;
+        System.arraycopy(values, 0, values_new, 0, values.length);
+        Instance inst = new DenseInstance(1.0, values_new);
+        inst.setDataset(getHeader());
+        return new InstanceExample(inst);
     }
 
     @Override
     public boolean isRestartable() {
-        return false;
+        return true;
     }
 
     @Override
     public void restart() {
-
+        numGeneratedInstances = 0;
     }
 
     @Override
     public void getDescription(StringBuilder stringBuilder, int i) {
 
+    }
+
+    private void addInstance(Instance instance) {
+        DataPoint point = new DataPoint(instance, numGeneratedInstances);
+        points.add(point);
+
+    }
+
+    protected void generateHeader() { // 2013/06/02: Noise label
+        ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+        for (int i = 0; i < this.numAttsOption.getValue(); i++) {
+            attributes.add(new Attribute("att" + (i + 1)));
+        }
+
+        ArrayList<String> classLabels = new ArrayList<String>();
+        for (int i = 0; i < this.numClusterOption.getValue(); i++) {
+            classLabels.add("class" + (i + 1));
+        }
+
+        attributes.add(new Attribute("class", classLabels));
+        streamHeader = new InstancesHeader(new Instances(getCLICreationString(InstanceStream.class), attributes, 0));
+        streamHeader.setClassIndex(streamHeader.numAttributes() - 1);
     }
 }
